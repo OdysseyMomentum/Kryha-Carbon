0bc4184ddc08ee3781b1062707c41be8f1bca058
Panos <panos@kryha.io>
2020-11-13 19:28:54 +0100

    blockchain + ganache setup + docker

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,39 +1,11 @@
-// module.exports = {
-//   networks: {
-//     development: {
-//       host: "127.0.0.1",
-//       port: 8545,
-//       network_id: "*",
-//     },
-//   },
-// };
-
-const { HD_WALLET_MNEMONIC, ETHEREUM_RPC_URL } = require("./constants.js");
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const { HOST, PORT } = require("./constants.js");
 
 module.exports = {
   networks: {
     development: {
-      provider: () =>
-        new HDWalletProvider(HD_WALLET_MNEMONIC, ETHEREUM_RPC_URL, 0, 10),
-      from: "0xe311775922ccb9e5cfc7dc94aa0399c0da695d02",
-      gas: 5500000,
+      host: HOST,
+      port: PORT,
       network_id: "*",
-    },
-  },
-
-  contracts_directory: "./contracts",
-  contracts_build_directory: "./build",
-
-  compilers: {
-    solc: {
-      version: "0.5.10",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200,
-        },
-      },
     },
   },
 };
