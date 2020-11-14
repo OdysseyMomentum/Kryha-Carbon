77a74be2cc46606137b80a9c2242846a962c5d33
Panos <panos@kryha.io>
2020-11-14 12:09:52 +0100

    Notary contract + fix mongo in docker-compose + fix host for ganache

--- blockchain/constants.js
+++ blockchain/constants.js
@@ -1,6 +1,6 @@
 require("dotenv-flow").config();
 
-const HOST = process.env.HOST || localhost;
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 8545;
 
 module.exports = {
