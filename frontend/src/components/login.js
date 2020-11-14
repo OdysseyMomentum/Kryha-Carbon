628ef56ae6213eecb7efd0a7cad85bcad252ef27
Timothe Dol van Wervershoof <timothedolvanwervershoof@Timothes-MacBook-Pro.local>
2020-11-14 16:11:15 +0100

    Style login page

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -7,6 +7,9 @@ import { useStoreContext } from "../context";
 import { ROUTES } from "../router/routes";
 import { login } from "../actions/user";
 
+import { color } from '../assets/color';
+import { ReactComponent as DownArrow } from '../assets/icons/down-arrow.svg';
+
 export const Login = () => {
     const [email, setEmail] = useState("");
 	const [{ user, availableUsers }, dispatch] = useStoreContext();
@@ -51,7 +54,7 @@ export const Login = () => {
                         {availableUsers.length
                             ? (
                                 <>
-                                    <option value="" disabled key="empty"></option>
+                                    <option value="" disabled key="empty">Choose a company</option>
                                     {availableUsers.map((availableUser) => (
                                         <option value={availableUser.email} key={availableUser.email}>
                                             {`${availableUser.name} - ${availableUser.accountType}`}
@@ -63,7 +66,11 @@ export const Login = () => {
                             <option value="" disabled key="empty users">Currently no users available</option>
                         )}
                     </select>
-                    <button type="submit">login</button>
+                    <Button type="submit" email={email}>
+                        login
+                        &nbsp;
+                        <DownArrow />
+                    </Button>
                 </Form>
             </FormContainer>
         </>
@@ -75,16 +82,32 @@ const Header = styled.div`
 	width: calc(536px + ((100% - 536px) / 2));
     height: 320px;
     overflow: hidden;
-
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
     img {
         z-index: 1;
         position: absolute;
         opacity: 0.85;
     }
-
+    
     h1, h2 {
         z-index: 2;
         position: relative;
+        margin: 0;
+        margin-left: 10%;
+    }
+
+    h1 {
+        color: ${color.green};
+        font-size: 100px;
+    }
+
+    h2 {
+        color: ${color.lightPurple};
+        font-weight: 700;
+        font-size: 40px;
     }
 `;
 
@@ -92,8 +115,84 @@ const FormContainer = styled.div`
 	width: 100%;
 	margin: 0 auto;
     max-width: 536px;
+
+    h6 {
+        color: ${color.green};
+        text-align: right;
+        font-weight: 500;
+        font-size: 12px;
+        line-height: 15px;
+        letter-spacing: 0.08em;
+        text-transform: uppercase;
+        margin: 18px 0;
+    }
+
+    hr {
+        border: 1px solid ${color.green};
+        width: 192px;
+        margin: 0 0 0 auto;
+    }
+
+    h3 {
+        font-size: 40px;
+        margin-bottom: 35px;
+        color: ${color.darkPurple};
+    }
 `;
 
 const Form = styled.form`
     width: 100%;
+    display: flex;
+    align-items: flex-end;
+    flex-direction: column;
+
+    select {
+        width: 100%;
+        border: 1px solid ${color.green};
+        border-radius: 3px;
+        padding: 16px;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        background: url(../assets/icons/down-arrow.svg) no-repeat right ${color.purple};
+
+        font-weight: 500;
+        font-size: 18px;
+        letter-spacing: 0.08em;
+        text-transform: uppercase;
+        color: ${color.green};
+
+        :focus {
+            outline: none;
+        }
+    }
+`;
+
+const Button = styled.button`
+    border-radius: 3px;
+    margin-top: 35px;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    font-size: 18px;
+    letter-spacing: 0.08em;
+    text-transform: uppercase;
+    color: ${(props) => props.email ? color.green : color.darkPurple};
+    background-color: ${color.purple};
+    border: 1px solid ${(props) => props.email ? color.green : color.darkPurple};
+    padding: 16px;
+    cursor: pointer;
+
+    svg {
+        height: 18px;
+        transform: rotate(-90deg);
+
+        path {
+            stroke: ${(props) => props.email ? color.green : color.darkPurple};
+        }
+    }
+
+    :focus {
+        outline: none;
+    }
 `;
